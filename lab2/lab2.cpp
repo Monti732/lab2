dbadc2bf6d7205baae1f326314893c79167e928f
Monti XD <proverka22808@outlook.com>
2024-09-20 17:42:31 +0700

    переделал

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 int main() {
 	float a, b, v, t;
-	cin >> a;
-	cin >> b;
-	cin >> t;
+	
+	cin >> a; // 0.0288
+	cin >> b; // 0.032
+	cin >> t; // 15.7
+	
 	for (v = 0.05; v <= 0.21; v += 0.05) {
 		float p = 0.0635 * t / (v - b) * 1.0 - a / pow(v, 2);
 		cout << v << "    " << p << endl;
 	}
+	
 	for (v = 0.2; v <= 1.1; v += 0.2) {
 		float p = 0.0635 * t / (v - b) * 1.0 - a / pow(v, 2);
 		cout << v << "    " << p << endl;
